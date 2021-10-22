3727b925ad237c828a13ffb69afd9ed2435b8c90
Andretreccia <andreabtreccia@gmail.com>
2021-10-22 15:30:28 +0200

    Creato un array con i membri del team

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+const alleanzaRibelle =
+    [
+        {
+            name: "Leila Organa",
+            role: "Dipolatico",
+            image: "https://static.wikia.nocookie.net/starwars/images/1/15/Leia_3.jpg/revision/latest/scale-to-width-down/650?cb=20191030220537&path-prefix=it"
+
+        },
+        {
+            name: "Ian Solo ",
+            role: "Generale",
+            image: "https://static.wikia.nocookie.net/starwars/images/c/c8/Epiv2.jpg/revision/latest?cb=20090402162704&path-prefix=it"
+
+        },
+        {
+            name: "Luke Skywalker",
+            role: "Comandante",
+            image: "https://i1.wp.com/starwarsitalia.net/wp-content/uploads/Star-Wars-Italia-Luke-Limpero-colpisce-ancora.png?resize=750%2C882&ssl=1"
+
+        },
+        {
+            name: "Lando Calrissian",
+            role: "Generale",
+            image: "https://static.wikia.nocookie.net/starwars/images/7/7d/Lando_WoSW.jpg/revision/latest?cb=20090621145846&path-prefix=it"
+
+        },
+        {
+            name: "Gial Ackbar ",
+            role: "Ammiraglio",
+            image: "https://static.wikia.nocookie.net/starwars/images/f/fb/Ackbar_HS.jpg/revision/latest?cb=20140105210611&path-prefix=it",
+        },
+
+    ]
+console.log(alleanzaRibelle)
